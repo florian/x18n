908b106167f05534910706005eb940531cd8b18d
Florian H <jscoderr@gmail.com>
2012-12-22 22:49:02 +0100

    add coffee task

--- grunt.js
+++ grunt.js
@@ -2,8 +2,21 @@ module.exports = function (grunt) {
 
 	grunt.initConfig({
 
+		meta: {
+			banner: '// ' + grunt.file.read('LICENSE').split("\n")[0]
+		},
+
+		coffee: {
+			compile: {
+				files: {
+					'lib/x18n.js': ['lib/observable.coffee', 'lib/x18n.coffee'],
+					'spec/spec.js': 'spec/spec.coffee'
+				}
+			}
+		}
+
 	});
 
-
+	grunt.loadNpmTasks('grunt-contrib-coffee');
 
 };
